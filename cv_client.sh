622e1380feeb9738daa1f7ad1a1b073e82c57ff7
Leco <60701459+L-e-c-o@users.noreply.github.com>
2021-01-26 23:29:47 +0100

    Update cv_client.sh

--- cv_client.sh
+++ cv_client.sh
@@ -9,15 +9,15 @@
 # Variables with same name in client and server script must have same value
 
 # Name of rsa key pair used to connect to vault (e.g. vault_key)
-SSH_KEY=#vault_key
+SSH_KEY=vault_key
 # Name of the user used to create the vault on server (e.g. admin)
-SSH_USER=#user
+SSH_USER=leco
 # IP of the server on which the vault is installed (e.g. 192.168.1.10)
-SSH_HOST=#172.16.57.129
+SSH_HOST="" #192.168.5.16
 # It's recommended to change default SSH port (e.g. 7222)
-SSH_PORT=#7222
+SSH_PORT=""
 # Name of the user created only for the vault (e.g. vault)
-VAULT_USER=#coffre
+VAULT_USER=coffre
 
 ############# CONSTANTS ##############
 
@@ -76,6 +76,39 @@ ${BOLD}IMPORTANT${NC}
 	exit
 }
 
+check_var() {
+	while [ -z "$VAULT_USER" ]
+	do
+		read -p "Enter vault user name : " VAULT_USER
+	done	
+	
+	while [ -z "$SSH_KEY" ]
+	do
+		read -p "Enter the name of the SSH key files : " SSH_KEY
+	done	
+	
+	while [ -z "$SSH_USER" ]
+	do
+		read -p "Enter SSH user name: " VAULT_USER
+	done	
+	
+	while [ -z "$SSH_HOST" ]
+	do
+		read -p "Enter server address : " SSH_HOST
+	done	
+	
+	# Check if the port number is valid / is note used
+	while [ -z "$SSH_PORT" ]
+    do
+        read -p "Enter server port : " SSH_PORT
+        if ! [[ "$SSH_PORT" =~ ^[0-9]+$ ]] || [ "$SSH_PORT" -lt 1 ] || [ "$SSH_PORT" -gt 65535 ]; then
+            error "Invalid port number or already in use"
+            SSH_PORT=""
+        fi
+    done
+
+}
+
 ############# MAIN ##############
 
 # ./cv_client.sh -h displays help message
@@ -83,17 +116,13 @@ if [ "$1" == "-h" ]; then
 	help
 fi
 
-# Check if variables are all set
-if [ -z "$SSH_HOST" ] || [ -z "$SSH_USER" ] || [ -z "$SSH_KEY" ] || [ -z "$SSH_PORT" ] || [ -z "$VAULT_USER" ]; then
-	error "Error try -h for help and check IMPORTANT section"
-	exit
-fi
-
 # Don't run this script as root, you will be prompted if sudo needed
 if [ "$EUID" -eq 0 ];then 
 	error "Don't run this script as root."
   	exit
 fi
+
+check_var
 
 case $1 in
 
