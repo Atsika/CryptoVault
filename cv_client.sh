9d185a0fed63abe0a622ae9623e27cf11d59c40e
Atsika <46649001+Atsika@users.noreply.github.com>
2021-01-27 19:54:58 +0100

    Update cv_client.sh

--- cv_client.sh
+++ cv_client.sh
@@ -141,7 +141,7 @@ Host VAULT
 	ssh -t VAULT "sudo cryptsetup luksOpen /dev/$VOLGROUP/$LOGVOLUME $ENC_LOGVOL && sudo mount /dev/mapper/$ENC_LOGVOL COFFRE"
 	mkdir $HOME/COFFRE
 	sshfs -o reconnect VAULT:COFFRE $HOME/COFFRE
-	sed -i 's/USER/$HOME/g' $HOME/COFFRE/MEMENTO/conf.yml
+	sed -i "s/USER/$HOME/g" $HOME/COFFRE/MEMENTO/conf.yml
 	sudo ln -s $HOME/COFFRE/MEMENTO/cheat /usr/sbin/cheat
 	info "Sourcing .bashrc"
 	source $HOME/COFFRE/ENVIRONNEMENT/bash/.bashrc
